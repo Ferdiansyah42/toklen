00cfd51008e5b8a11e313a2e00026243137156dd
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-22 02:05:43 +0700

    perbaikan

--- components/ListMusic.tsx
+++ components/ListMusic.tsx
@@ -1,9 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { typeMusic } from '@/data/typemusic'
 
+interface Props{
+    musicData: typeMusic[];
+}
 
-
-const ListMusic = ({ musicData }) => {
+const ListMusic = ({ musicData }: Props) => {
   return (
     <View className='flex h-screen items-center justify-center'>
         <Text className='text-3xl font-semibold'>Aplikasi Music</Text>
