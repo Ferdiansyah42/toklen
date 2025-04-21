9b1b2753462bf37ed73009c0da4784ce964e5c79
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-22 02:33:08 +0700

    icon arow

--- components/Playing.tsx
+++ components/Playing.tsx
@@ -1,12 +1,17 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Ionicons } from "@expo/vector-icons";
 
 const Playing = () => {
   return (
-    <View className='flex h-screen items-center justify-center'>
-        <Text className='text-3xl font-semibold'>Aplikasi Music</Text>
+    <View style={{ height: '100%' }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 7, alignItems: 'center', marginTop: 7 }}>
+            <View style={{ padding: 4, borderRadius: 100, backgroundColor: '#333', borderColor: '#333', shadowColor: 'white', shadowOpacity: 0.5 }}>
+                <Ionicons name="arrow-back" size={24} color={"white"} />
+            </View>
+        </View>
     </View>
-  )
-}
+  );
+};
 
-export default Playing
+export default Playing;
