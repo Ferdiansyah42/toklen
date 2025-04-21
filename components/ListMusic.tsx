2a63a6f99913176fa68f1ee04532af422e5d27cb
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-22 00:41:02 +0700

    file tsx

---
+++ components/ListMusic.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListMusic = () => {
+  return (
+    <View>
+      <Text>ListMusic</Text>
+    </View>
+  )
+}
+
+export default ListMusic
