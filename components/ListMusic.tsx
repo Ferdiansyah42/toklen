fed8259979c6b1732063c68c5b197547d99fa3ab
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-22 02:02:51 +0700

    nambah type music

--- components/ListMusic.tsx
+++ components/ListMusic.tsx
@@ -1,7 +1,9 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const ListMusic = () => {
+
+
+const ListMusic = ({ musicData }) => {
   return (
     <View className='flex h-screen items-center justify-center'>
         <Text className='text-3xl font-semibold'>Aplikasi Music</Text>
---
+++ data/typeMusic.ts
@@ -0,0 +1,7 @@
+export type typeMusic = {
+    title: string;
+    artist: string;
+    artwork: string;
+    url: string;
+    id: string;
+};
