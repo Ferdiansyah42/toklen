95ab8318b4f886a542227c862b1984649caa903a
Ferdiansyah <ferdiansyah.fr42@gmail.com>
2025-04-22 00:21:20 +0700

    layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import "../global.css";
 
 const _layout = () => {
   return (
-    <View>
+    <View className='flex h-screen items-center justify-center'>
       <Text>_layout</Text>
     </View>
   )
